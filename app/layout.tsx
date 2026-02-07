bf13a715e054f2fda52868c02c5b8eb4d0917950
ahadsheikh1814 <ahadsheikh1814@outlook.com>
2026-02-07 13:00:50 +0600

    Update favicon in layout metadata for consistent branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: 'Treefy | All Your Links.One Smart Tree.',
   description: ' Share everything you create with one simple Treefy link.',
-  icons: { icon: '/Treefy.ico' },
+  icons: { icon: '/favicon.ico' },
 }
 
 export default function RootLayout({
